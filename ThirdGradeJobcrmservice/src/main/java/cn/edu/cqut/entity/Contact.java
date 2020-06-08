10d0cc25d620776ba697cfae589cd5effe5db9ad
xieyan <2401558728@qq.com>
2020-06-08 22:50:03 +0800

    123

---
+++ ThirdGradeJobcrmservice/src/main/java/cn/edu/cqut/entity/Contact.java
@@ -0,0 +1,115 @@
+package cn.edu.cqut.entity;
+
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableField;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author CQUT SE 2020
+ * @since 2020-06-08
+ */
+public class Contact extends Model<Contact> {
+
+    private static final long serialVersionUID=1L;
+
+    @TableId("ctId")
+    private String ctId;
+
+    @TableField("ctName")
+    private String ctName;
+
+    @TableField("ctGender")
+    private String ctGender;
+
+    @TableField("ctPhone")
+    private String ctPhone;
+
+    @TableField("ctTitle")
+    private String ctTitle;
+
+    @TableField("ctMemo")
+    private String ctMemo;
+
+    @TableField("cusNo")
+    private String cusNo;
+
+
+    public String getCtId() {
+        return ctId;
+    }
+
+    public void setCtId(String ctId) {
+        this.ctId = ctId;
+    }
+
+    public String getCtName() {
+        return ctName;
+    }
+
+    public void setCtName(String ctName) {
+        this.ctName = ctName;
+    }
+
+    public String getCtGender() {
+        return ctGender;
+    }
+
+    public void setCtGender(String ctGender) {
+        this.ctGender = ctGender;
+    }
+
+    public String getCtPhone() {
+        return ctPhone;
+    }
+
+    public void setCtPhone(String ctPhone) {
+        this.ctPhone = ctPhone;
+    }
+
+    public String getCtTitle() {
+        return ctTitle;
+    }
+
+    public void setCtTitle(String ctTitle) {
+        this.ctTitle = ctTitle;
+    }
+
+    public String getCtMemo() {
+        return ctMemo;
+    }
+
+    public void setCtMemo(String ctMemo) {
+        this.ctMemo = ctMemo;
+    }
+
+    public String getCusNo() {
+        return cusNo;
+    }
+
+    public void setCusNo(String cusNo) {
+        this.cusNo = cusNo;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.ctId;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+        "ctId=" + ctId +
+        ", ctName=" + ctName +
+        ", ctGender=" + ctGender +
+        ", ctPhone=" + ctPhone +
+        ", ctTitle=" + ctTitle +
+        ", ctMemo=" + ctMemo +
+        ", cusNo=" + cusNo +
+        "}";
+    }
+}
