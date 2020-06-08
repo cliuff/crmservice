97e730a79dacf620ab844e9e3f85514b37875eb4
15730366181 <1154690052@qq.com>
2020-06-08 10:30:14 +0800

    45868

---
+++ crmservice/src/main/java/cn/edu/cqut/mapper/CustomerMapper.java
@@ -0,0 +1,16 @@
+package cn.edu.cqut.mapper;
+
+import cn.edu.cqut.entity.Customer;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author HQYJ
+ * @since 2020-06-03
+ */
+public interface CustomerMapper extends BaseMapper<Customer> {
+
+}
