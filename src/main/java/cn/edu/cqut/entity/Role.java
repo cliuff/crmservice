a37950d7265e1f63726ad125d58abdab326ca049
cliuff <35420834+cliuff@users.noreply.github.com>
2020-06-10 09:02:01 +0800

    role 相关类

---
+++ src/main/java/cn/edu/cqut/entity/Role.java
@@ -0,0 +1,88 @@
+package cn.edu.cqut.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author CQUT SE 2020
+ * @since 2020-06-10
+ */
+public class Role extends Model<Role> {
+
+    private static final long serialVersionUID=1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String roleName;
+
+    private String description;
+
+    private Integer isRun;
+
+    private LocalDateTime createTime;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getIsRun() {
+        return isRun;
+    }
+
+    public void setIsRun(Integer isRun) {
+        this.isRun = isRun;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+        "id=" + id +
+        ", roleName=" + roleName +
+        ", description=" + description +
+        ", isRun=" + isRun +
+        ", createTime=" + createTime +
+        "}";
+    }
+}
---
+++ src/main/java/cn/edu/cqut/service/impl/RoleServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.edu.cqut.service.impl;
+
+import cn.edu.cqut.entity.Role;
+import cn.edu.cqut.mapper.RoleMapper;
+import cn.edu.cqut.service.IRoleService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author CQUT SE 2020
+ * @since 2020-06-10
+ */
+@Service
+public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements IRoleService {
+
+}
